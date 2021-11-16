f9b5e1945935a1129bcb672e4e3ea517f5affc4c
andiazo <andiazo@unal.edu.co>
2021-11-15 22:14:21 -0500

    Testing SonarCloud

--- deletethistest2.py
+++ deletethistest2.py
@@ -1,1 +1,31 @@
 print("hola mundo")
+
+methods = []                       
+for i in range(10):                        
+    methods.append(lambda x: x + i)        
+
+print methods[0](10)    
+
+sizes = [
+    {
+        "name": "item_1",
+        "size_in_mL": "100mL",
+        "alternate_identifier": "axc45"
+    },
+    {
+        "name": "item_2",
+        "size_in_mL": "100mL",
+        "alternate_identifier": "axc675"
+    },
+    {
+        "name": "item_2",
+        "size_in_mL": "400mL",
+        "alternate_identifier": "axc66725"
+    },
+    {
+        "name": "item_3",
+        "size_in_mL": "50mL",
+        "alternate_identifier": "axd10025"
+    }
+
+]
