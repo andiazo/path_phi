186d921e9cb00bd1118d96172b9c52ae3ee0d9c7
caaninome1 <caaninome@unal.edu.co>
2021-12-06 20:46:59 -0500

    eliminación de archivos antiguos

--- api/entities/user.js
+++
@@ -1,9 +0,0 @@
-export default function user(username, password, email, role, createdAt) {
-    return {
-      getUserName: () => username,
-      getPassword: () => password,
-      getEmail: () => email,
-      getRole: () => role,
-      getCreatedAt: () => createdAt
-    };
-  }
