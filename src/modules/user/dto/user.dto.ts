06391d0d20c13803ed681cb35755b0504ff1d0e8
caaninome1 <caaninome@unal.edu.co>
2021-12-06 20:56:20 -0500

    commit inicial

---
+++ src/modules/user/dto/user.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty } from "class-validator";
+import { RoleType } from "src/modules/role/roletype.enum";
+import { UserDetails } from "../user.details.entity";
+export class UserDto {
+  @IsNotEmpty()
+  id: number;
+
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  roles: RoleType[]
+
+  @IsNotEmpty()
+  details: UserDetails;
+}
