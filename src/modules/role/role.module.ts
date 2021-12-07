673f0c3fa2ff3ebd8e6f2b196a47f7b813fef352
caaninome1 <caaninome@unal.edu.co>
2021-12-06 21:44:37 -0500

    agregado role controller, entity , repo

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleRepository } from './rol.repository';
+import { RoleController } from './role.controller';
+import { RoleService } from './role.service';
 
-@Module({})
-export class RoleModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([RoleRepository])],
+  providers: [RoleService],
+  controllers: [RoleController],
+})
+export class RoleModule { }
---
+++ src/modules/role/role.service.ts
@@ -0,0 +1,53 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from './role.entity';
+import { RoleRepository } from './rol.repository';
+@Injectable()
+export class RoleService {
+  constructor(
+    @InjectRepository(RoleRepository)
+    private readonly _roleRepository: RoleRepository,
+  ) { }
+
+  async get(id: number): Promise<Role> {
+    if (!id) {
+      throw new BadRequestException("id must be sent");
+    }
+    const role: Role = await this._roleRepository.findOne(id, {
+      where: { status: 'ACTIVE' },
+    });
+
+    if (!role) {
+      throw new NotFoundException("Role does not exist");
+    }
+
+    return role;
+  }
+
+  async getAll(): Promise<Role[]> {
+    const roles: Role[] = await this._roleRepository.find({
+      where: { status: 'ACTIVE' },
+    });
+
+    return roles;
+  }
+
+  async create(role: Role): Promise<Role> {
+    const savedRole = await this._roleRepository.save(role);
+    return savedRole;
+  }
+
+  async update(id: number, role: Role): Promise<void> {
+    await this._roleRepository.update(id, role);
+  }
+
+  async delete(id: number): Promise<void> {
+    const roleExists = await this._roleRepository.findOne(id, {
+      where: { status: 'ACTIVE' },
+    });
+    if (!roleExists) {
+      throw new NotFoundException("Role does not exist");
+    }
+    await this._roleRepository.update(id, { status: 'INACTIVE' });
+  }
+}
