ccb882ad74f8cc95a95e8532e7dd36c9e592bec0
Andres Diaz <andiazo@unal.edu.co>
2022-02-03 02:24:37 -0500

    aws

--- src/HTTPscripts/learningPathsScripts.js
+++ src/HTTPscripts/learningPathsScripts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const defaultURL = "http://localhost:5000/api/"
+const defaultURL = "http://3.128.170.111:5000/api"
 
 export function getAllPaths(setPaths){
     axios.get(`${defaultURL}learning-path/`)
