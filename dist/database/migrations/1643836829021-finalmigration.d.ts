52a0bdd9e841aee6f16983656290f4114886f628
Andres Diaz <andiazo@unal.edu.co>
2022-02-03 01:33:18 -0500

    conf

---
+++ dist/database/migrations/1643836829021-finalmigration.d.ts
@@ -0,0 +1,6 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+export declare class finalmigration1643836829021 implements MigrationInterface {
+    name: string;
+    up(queryRunner: QueryRunner): Promise<void>;
+    down(queryRunner: QueryRunner): Promise<void>;
+}
