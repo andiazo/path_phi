4cf413e0d7288ef6fc3fa6e76128a703fe2987cd
dicarvajalb <dicarvajalb@unal.edu.co>
2022-02-07 20:10:09 -0500

    Rutas de consulta actualizadas

--- src/HTTPscripts/authScripts.js
+++ src/HTTPscripts/authScripts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const defaultURL = "http://localhost:5000/api/"
+const defaultURL = "http://3.15.144.53:5000/api/"
 
 export function signUp(setIsRegistered, setAlertMessage, userToRegister){
     axios.post(`${defaultURL}auth/signup`, userToRegister)
--- src/HTTPscripts/learningPathsScripts.js
+++ src/HTTPscripts/learningPathsScripts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const defaultURL = "http://localhost:5000/api/"
+const defaultURL = "http://3.15.144.53:5000/api/"
 
 export function getAllPaths(setPaths){
     axios.get(`${defaultURL}learning-path/`)
